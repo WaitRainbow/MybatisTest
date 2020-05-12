a4758f0f1fc235fdc664d5ebef3f726c4553d4e2
WaitRainbow <838344436@qq.com>
2020-05-12 16:50:24 +0800

    Test for github's branch

--- src/main/java/com/cc/domain/Test.java
+++ src/main/java/com/cc/domain/Test.java
@@ -3,5 +3,6 @@ package com.cc.domain;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Just a test for github's branch");
+        System.out.println("hhhh");
     }
 }
